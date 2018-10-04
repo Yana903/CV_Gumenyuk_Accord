1429cc5efd156a328740edc8b137a02b4ba6e349
Yana903 <gumenyuk903@mail.com>
2018-10-04 20:28:53 +0300

    first commit

---
+++ js/Accordeon.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.accordeon-trigger').on('click', function(e){
+        e.preventDefault();
+
+        var $this = $(this),
+            item = $this.closest('.accordeon-item'),
+            list = $this.closest('.accordeon-list'),
+            items = list.find('.accordeon-item'),
+            content = item.find('.accordeon-inner'),
+            otherContent = list.find('.accordeon-inner'),
+            duration = 300;
+
+            if(!item.hasClass('active')){
+                // items.removeClass('active');
+                item.addClass('active');
+
+                otherContent.stop(true, true).slideUp(duration);
+                content.stop(true, true).slideDown(duration);
+            }
+            else{
+                content.stop(true, true).slideUp(duration);
+                item.stop(true, true).removeClass('active');
+            }
+
+    });
+});
